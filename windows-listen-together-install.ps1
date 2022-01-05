0e226488561b5b9e82e7d8a38b366d5c171e0b1a
FlafyDev <flafyarazi@gmail.com>
2022-01-06 00:47:46 +0200

    changed icon a little improved windows install script

--- windows-listen-together-install.ps1
+++ windows-listen-together-install.ps1
@@ -1,4 +1,9 @@
-Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
+if (Get-Command Spicetify -errorAction SilentlyContinue) {
+  Write-Output 'Spicetify already installed.'
+} else {
+  Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
+}
 spicetify config extensions listenTogether.js
 Invoke-WebRequest "https://github.com/FlafyDev/spotify-listen-together/releases/download/vX/listenTogether.js" -OutFile ($( Split-Path $( spicetify -c ) ) + "\Extensions\listenTogether.js")
 spicetify backup apply
+spicetify apply
